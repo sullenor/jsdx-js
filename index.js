a1ba63029a6c3c4f21021d1d467181cd04215616
Alexey Litvinov <iamhoneyboy@gmail.com>
2014-10-27 01:04:55 +0300

    initial

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var parseJs = require('./lib/parse-js');
+var transform = require('./lib/transform');
+
+/**
+ * Парсит строку, возвращает синтаксическое дерево.
+ * В противном случае вернет null.
+ *
+ * @param  {String} string
+ * @param  {Object} options
+ * @param  {String} options.context  Имя блока.
+ * @param  {Object} options.filename
+ * @return {Object}
+ */
+module.exports = function (string, options) {
+    return transform(parseJs(string), options);
+};
