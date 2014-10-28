117650a5dbeafe87540bf03be19258ffba594055
Alexey Litvinov <iamhoneyboy@gmail.com>
2014-10-29 00:31:28 +0300

    Шаблончики для будущих тестов

---
+++ test/parse-tags.js
@@ -0,0 +1,8 @@
+describe('parse-tags.js', function () {
+    'use strict';
+
+    var expect = require('expect.js');
+    var parseTags = require('../lib/parse-tags');
+
+    it('', function () {});
+});
---
+++ test/transform.js
@@ -0,0 +1,26 @@
+describe('transform.js', function () {
+    'use strict';
+
+    var expect = require('expect.js');
+    var transform = require('../lib/transform');
+
+    describe('parseArguments()', function () {
+        it('', function () {});
+    });
+
+    describe('parseComments()', function () {
+        it('', function () {});
+    });
+
+    describe('parseMethods()', function () {
+        it('', function () {});
+    });
+
+    describe('parseName()', function () {
+        it('', function () {});
+    });
+
+    describe('transform()', function () {
+        it('', function () {});
+    });
+});
